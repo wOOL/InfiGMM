e21c6193328359026975f9052277fd19acb994f9
Yongxin Yang <iamwool@gmail.com>
2013-11-18 23:02:51 +0000

    hmm

--- plotF.m
+++ plotF.m
@@ -1,10 +1,10 @@
-function plotF(data, z)
+function plotF(X, Z)
 
     S = ['d','o','x','+','.','v','^'];
     C = ['y','m','c','r','g','b','k'];
-    for i = min(z):max(z)
+    for i = min(Z):max(Z)
         hold on;
-        scatter(data(z==i,1),data(z==i,2),S(mod(i,length(S))+1),C(mod(i,length(C))+1));
+        scatter(X(Z==i,1),X(Z==i,2),S(mod(i,length(S))+1),C(mod(i,length(C))+1));
     end
     legend;
     
